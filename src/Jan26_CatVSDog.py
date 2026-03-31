8f70bd2df6a5e1385fd2c357171eef128bac4967
DarinAnthony <darinanthonyad@gmail.com>
2026-03-30 18:41:14 -0700

    updated path

--- src/Jan26_CatVSDog.py
+++ src/Jan26_CatVSDog.py
@@ -3,7 +3,33 @@ from fastai.vision.all import load_learner, PILImage
 from PIL import Image
 import io
 
-# ✅ Must exist when unpickling the Learner
+
+"""
+import numpy
+import scipy
+import matplotlib
+import torch
+import fastai
+import PIL
+
+print("numpy:", numpy.__version__)
+print("scipy:", scipy.__version__)
+print("matplotlib:", matplotlib.__version__)
+print("torch:", torch.__version__)
+print("fastai:", fastai.__version__)
+print("pillow (PIL):", PIL.__version__)
+
+OUTPUT:
+numpy: 2.0.2
+scipy: 1.15.3
+matplotlib: 3.10.0
+torch: 2.8.0
+fastai: 2.8.4
+pillow (PIL): 11.3.0
+"""
+
+
+# Must exist when unpickling the Learner
 def cat_or_dog(file_name):
     # In training, file_name was something like "Siamese_20.jpg"
     # At inference, it may be a Path. Convert to just the base name.
@@ -14,7 +40,7 @@ st.set_page_config(page_title="Cat vs Dog Classifier", layout="centered")
 
 @st.cache_resource
 def get_model():
-    return load_learner("cat_vs_dog_model.pkl")
+    return load_learner("models/cat_vs_dog_model_fastai_2_8_4_rn50.pkl")
 
 learn = get_model()
 
